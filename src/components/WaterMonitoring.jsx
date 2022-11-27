568fdb78b6c8ec5644038844a423c4d46beec3bf
rakesh <rakeshhotker@gmail.com>
2022-11-27 17:47:41 +0530

    few more ups

--- src/components/WaterMonitoring.jsx
+++ src/components/WaterMonitoring.jsx
@@ -3,13 +3,12 @@ import React, { useEffect, useState } from "react";
 
 import Chart from "react-apexcharts";
 
-const Mycharts = () => {
+const Mycharts = ({ nodeid }) => {
   const [tds, settds] = useState([]);
   const [date, setDate] = useState([]);
   useEffect(() => {
     const getData = async () => {
-      const url =
-        "https://iudx-rs-onem2m.iiit.ac.in/ngsi-ld/v1/temporal/entities?id=research.iiit.ac.in/4786f10afbf48ed5c8c7be9b4d38b33ca16c1d9a/iudx-rs-onem2m.iiit.ac.in/iiith-water-monitoring/WM-WD-VN00-00&limit=2000&time=2022-11-17T17:50:00Z&timerel=during&endtime=2022-11-24T17:50:00Z";
+      const url = `https://iudx-rs-onem2m.iiit.ac.in/ngsi-ld/v1/temporal/entities?id=research.iiit.ac.in/4786f10afbf48ed5c8c7be9b4d38b33ca16c1d9a/iudx-rs-onem2m.iiit.ac.in/iiith-water-monitoring/${nodeid}&limit=2000&time=2022-11-17T17:50:00Z&timerel=during&endtime=2022-11-24T17:50:00Z`;
       try {
         const response = await fetch(url);
         const data = await response.json();
@@ -64,9 +63,25 @@ function WaterMonitoring() {
           <WaterMonitoringInstant />
         </div>
         <div className="flex flex-col items-center justify-center w-full">
-          <h1 className="text-center">Temporal display(TDS)</h1>
+          <label
+            for="nodeid"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select an option
+          </label>
+          <select
+            id="nodeid"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Choose a NodeId</option>
+            <option value="WM-WD-VN00-00">WM-WD-VN00-00</option>
+            <option value="WM-WD-VN90-00">WM-WD-VN90-00</option>
+            <option value="WM-WD-VN01-01">WM-WD-VN01-01</option>
+            <option value="WM-WD-VN00-00">WM-WD-VN00-00</option>
+          </select>
 
-          <Mycharts />
+          <h1 className="text-center">Temporal display(TDS)</h1>
+          <Mycharts nodeid={"WM-WD-VN00-00"} />
         </div>
       </div>
     </>
